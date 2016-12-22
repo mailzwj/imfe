2fcc8be234c08d0e474d34b156bba69080d8c65a
zhengwujiang <cdzhengwujiang@jd.com>
2016-12-22 15:44:22 +0800

    代码重构，规范代码格式

--- imfe.js
+++ imfe.js
@@ -1,5 +1,8 @@
-var cmd = require('./build');
+var cmd = require('commander');
 // console.log(cmd);
+
+require('./commander')(cmd);
+require('./build')(cmd);
 require('./plugin')(cmd);
 
 cmd.parse(process.argv);
